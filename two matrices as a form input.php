dc1cf5fda21435aab66992174d98dc98ab5906ff
Iyad Kokari <121595352+iyadgit55@users.noreply.github.com>
2024-07-10 23:55:26 +0400

    Update two matrices as a form input.php

--- two matrices as a form input.php
+++ two matrices as a form input.php
@@ -1,6 +1,46 @@
 <?php
-  echo "Today's Date is: ";
-  echo date("d/m/Y");
-  echo "<br>Current Time is: ";  
-  echo date("h:i:sa");
+echo "<form method='get'>
+<input type='submit' name='submit' value='MATRIX ADDITION'>
+</form>";
+if(isset($_GET['submit']))   
+{
+   $a=array(
+            array(1,2,3),
+            array(1,2,3),
+            array(1,2,3)
+         );
+   $b=array(
+            array(1,2,3),
+            array(1,2,3),
+            array(1,2,3)
+         );
+   print "\n Matrix 1: <br>";
+   for($i=0;$i<3;$i++)
+     {
+      for($j=0;$j<3;$j++)
+      {
+         echo $a[$i][$j],"  ";
+      }
+      echo "<br>";
+      }
+   echo "\n Matrix 2: <br>";
+   for($i=0;$i<3;$i++)
+     {
+      for($j=0;$j<3;$j++)
+      {
+         echo $b[$i][$j],"  ";
+      }
+      echo "<br>";
+      }   
+   $sum=array();
+   echo "\n Sum of Matrices: <br>";
+   for($i=0;$i<3;$i++)
+     {
+      for($j=0;$j<3;$j++)
+      {
+         echo $a[$i][$j]+$b[$i][$j],"  ";
+      }
+      echo "<br>";
+     }   
+}
 ?>
