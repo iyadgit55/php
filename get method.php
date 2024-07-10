6a6056061f5f279364576af0cd9d0dadd82fab7a
Iyad Kokari <121595352+iyadgit55@users.noreply.github.com>
2024-07-10 23:58:21 +0400

    Update get method.php

--- get method.php
+++ get method.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html>
+HTML File
 <html>
 <body>
-    <form action="PostDemo.php" method="post">
-        Username: <input type="text" name="uname"><br>
-        Password: <input type="password" name="pwd"><br>
-        <input type="submit" value="Login">
-    </form>
+<form action="GetDemo.php" method="get">
+Name	:<input type="text" name="name"><br>
+E-Mail	:<input type="text" name="email"><br>
+<input type="submit">
+</form>
 </body>
 </html>
+PHP File
+<?php
+echo "Hi ",$_GET['name'], "<br>";
+echo "Your email address is:" , $_GET['email']; 
+?>
