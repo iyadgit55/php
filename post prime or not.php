e2e978a8f54779596b2a3674ed2f09736bd2b2fe
Iyad Kokari <121595352+iyadgit55@users.noreply.github.com>
2024-07-10 23:59:25 +0400

    Update post prime or not.php

--- post prime or not.php
+++ post prime or not.php
@@ -1,17 +1,25 @@
-<!DOCTYPE html>
+HTML Code
 <html>
 <body>
-    <form action="display_string.php" method="get">
-        Enter a String: <input type="text" name="str"><br>
-        <input type="submit" value="Display">
-    </form>
+<form action="Prime.php" method="post">
+Ener a Number to Check Prime:<input type="text" name="Prime"><br>
+<input type="submit" value="CHECK">
+</form>
 </body>
 </html>
+PHP Code
 <?php
-if (isset($_GET['str'])) {
-    $enteredString = htmlspecialchars($_GET['str']);
-    echo "The entered string is: " . $enteredString;
-} else {
-    echo "No string entered.";
-}
+$n=$_POST['Prime'];
+$flag=0;
+for($i=2;$i<=$n/2;$i++)
+   { if($n % $i==0)
+         {
+           $flag=1;
+           break;
+         }
+   }
+if($flag==0)
+   echo $n, " is Prime";
+else
+   echo $n, " is Not Pime";  
 ?>
