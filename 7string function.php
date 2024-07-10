f703c7997387b5ab2fbe3be93278f5b0fcedeb85
Iyad Kokari <121595352+iyadgit55@users.noreply.github.com>
2024-07-10 23:02:48 +0400

    Update 7string function.php

--- 7string function.php
+++ 7string function.php
@@ -1,9 +1,17 @@
 <?php
-$originalArray = array("apple", "banana", "cherry", "date");
-$newItem = "orange";
-$insertPosition = 2;
-$index = $insertPosition - 1;
-array_splice($originalArray, $index, 0, $newItem);
-echo "Original Array after insertion: <br>";
-print_r($originalArray);
+$string = "Hello, World! This is a PHP string functions demonstration.";
+$length = strlen($string);
+echo "1. Length of the string: $length\n";
+$replaced = str_replace("World", "PHP", $string);
+echo "2. Replaced string: $replaced\n";
+$upper = strtoupper($string);
+echo "3. Uppercase string: $upper\n";
+$lower = strtolower($string);
+echo "4. Lowercase string: $lower\n";
+$substring = substr($string, 7, 5);
+echo "5. Substring: $substring\n";
+$position = strpos($string, "PHP");
+echo "6. Position of 'PHP' in the string: $position\n";
+$reversed = strrev($string);
+echo "7. Reversed string: $reversed\n";
 ?>
