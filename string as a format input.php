fa58fd1ba9f4ddb52fd8e94d9fbad1249afc4799
Iyad Kokari <121595352+iyadgit55@users.noreply.github.com>
2024-07-11 00:01:34 +0400

    Update string as a format input.php

--- string as a format input.php
+++ string as a format input.php
@@ -1,44 +1,13 @@
+HTML Code
+<html>
+<body>
+<form action="String.php" method="get">
+Enter a String	:<input type="text" name="str">
+<input type="submit" value="Display">
+</form>
+</body>
+</html>
+PHP Code
 <?php
-echo "<form method='get'>
-<input type='submit' name='submit' value='MATRIX ADDITION'>
-</form>";
-
-if (isset($_GET['submit'])) {
-    $a = [
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3]
-    ];
-    $b = [
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3]
-    ];
-
-    echo "<h3>Matrix 1:</h3>";
-    displayMatrix($a);
-
-    echo "<h3>Matrix 2:</h3>";
-    displayMatrix($b);
-
-    echo "<h3>Sum of Matrices:</h3>";
-    $sum = addMatrices($a, $b);
-    displayMatrix($sum);
-}
-
-function displayMatrix($matrix) {
-    foreach ($matrix as $row) {
-        echo implode(" ", $row) . "<br>";
-    }
-}
-
-function addMatrices($matrix1, $matrix2) {
-    $result = [];
-    for ($i = 0; $i < count($matrix1); $i++) {
-        for ($j = 0; $j < count($matrix1[$i]); $j++) {
-            $result[$i][$j] = $matrix1[$i][$j] + $matrix2[$i][$j];
-        }
-    }
-    return $result;
-}
+echo "The entered String is: ",$_GET['str'];
 ?>
